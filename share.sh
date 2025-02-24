52a128ef4e375f66591faf684f7cc030c1b35076
n2lentertainement <115657122+n2lentertainement@users.noreply.github.com>
2025-02-25 04:35:34 +0700

    Create share.sh

---
+++ share.sh
@@ -0,0 +1,4 @@
+docker run --name repocket -e RP_EMAIL=johnhoang@hvnteam.com -e RP_API_KEY=9106d586-2653-480d-80ad-14fe6692a5f5 -d --restart=always repocket/repocket
+sudo docker stop watchtower; sudo docker rm watchtower; sudo docker rmi containrrr/watchtower; sudo docker stop earnfm-client; sudo docker rm earnfm-client; sudo docker rmi earnfm/earnfm-client:latest; sudo docker run -d --restart=always -e EARNFM_TOKEN="053ef290-da8f-40f2-bd33-bc2a1eef9a0e" --name earnfm-client earnfm/earnfm-client:latest && sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 earnfm-client
+docker run -d --name tm traffmonetizer/cli_v2 start accept --token hQGZw0pjXJBcEMb6hp5cClJaa0rbu1zERwtvlGfyWcQ=
+docker run --restart unless-stopped packetshare/packetshare -accept-tos -email=johnhoang@hvnteam.com -password=Matkhau123
